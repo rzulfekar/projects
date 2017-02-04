f72aa2811212895b7bf98edba62aaa9afc8d53dc
Rex Zulfekar <rzulfekar@gmail.com>
2017-02-04 17:59:30 -0500

    exercise from karat

--- SandboxConsole/SandboxConsole/Program.cs
+++ SandboxConsole/SandboxConsole/Program.cs
@@ -9,10 +9,23 @@ namespace SandboxConsole
     class Program
     {
         static void Main(string[] args)
-        {           
-            // testing a comment
-            Console.WriteLine("Hello, Github!");
+        {
+            int num = 8;
+
+            //int answer = Solution(4, 5);
+
+            int[] a = {5,5,1,7,12,3,5 };
+
+            //int answer = solution1(5, a);
+
             Console.ReadLine();
+
+
+
         }
+
+
+        
+        
     }
 }
