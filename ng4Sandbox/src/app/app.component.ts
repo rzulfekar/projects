a83f8c50f4a465dea6e7636fe2152cc05603fd73
rzulfekar <rzulfekar@gmail.com>
2017-04-26 21:30:30 -0400

    beginning of styling pocs

--- ng4Sandbox/src/app/app.component.ts
+++ ng4Sandbox/src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'app works...!';
+    data = {
+      "Person" : 
+        [
+          {
+            "name" : "rex",
+            "age" : "42"
+          },
+          {
+            "name" : "jenn",
+            "age" : "42"
+          },
+          {
+            "name" : "jaz",
+            "age" : "8"
+          }
+        ]
+    }
+
+
+
+    constructor() {
+      debugger;
+        console.log("hello world" + this.data);
+    }
+
+    getClass(person: any) : string{
+        return "my-class";
+    }
+
+
 }
